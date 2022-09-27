0783ecf66a17aa952d815ecc359b17fbf5709735
truongnx <truongnx28111994@gmail.com>
2022-09-28 04:33:39 +0700

    E

--- resources/js/modals.js
+++ resources/js/modals.js
@@ -1,23 +1,30 @@
-import {Modal} from 'bootstrap';
 
-let modalsElement = document.getElementById('laravel-livewire-modals');
+// var Modal = $.fn.modal;
+// let modalsElement = document.getElementById('laravel-livewire-modals');
 
-modalsElement.addEventListener('hidden.bs.modal', () => {
+if ($.fn.modal == undefined) {
+    console.error("Chưa có thư viện Modal.js hoặc bootstrap chưa hỗ trợ");
+}
+
+// modalsElement.addEventListener('hidden.bs.modal', () => {
+//     Livewire.emit('resetModal');
+// });
+
+$('#laravel-livewire-modals').on('hidden.bs.modal', function (e) {
     Livewire.emit('resetModal');
 });
 
 Livewire.on('showBootstrapModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    if (!modal) {
-        modal = new Modal(modalsElement);
-    }
-
-    modal.show();
+    // let modal = $.fn.modal.getInstance(modalsElement);
+    // if (!modal) {
+    //     modal = new Modal(modalsElement);
+    // }
+    // modal.show();
+    $('#laravel-livewire-modals').modal('show');
 });
 
 Livewire.on('hideModal', () => {
-    let modal = Modal.getInstance(modalsElement);
-
-    modal.hide();
+    // let modal = Modal.getInstance(modalsElement);
+    // modal.hide();
+    $('#laravel-livewire-modals').modal('hide');
 });
